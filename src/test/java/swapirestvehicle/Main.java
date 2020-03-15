df6f1afcd14407cb8cd08d1e0a220b35132c32c4
Douglas Raymond Siano <DouglasSiano@gmail.com>
2020-03-15 15:51:56 -0400

    fix pom

--- src/test/java/swapirestvehicle/Main.java
+++ src/test/java/swapirestvehicle/Main.java
@@ -5,11 +5,19 @@ import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
+// https://swapi.co/api/vehicles/
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@Execution(ExecutionMode.CONCURRENT)
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
