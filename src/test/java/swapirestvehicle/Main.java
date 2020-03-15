ee2895271b960276cb879be47d87510cdc5fd123
Douglas Raymond Siano <DouglasSiano@gmail.com>
2020-03-15 15:04:13 -0400

    Project init

---
+++ src/test/java/swapirestvehicle/Main.java
@@ -0,0 +1,15 @@
+package swapirestvehicle;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
