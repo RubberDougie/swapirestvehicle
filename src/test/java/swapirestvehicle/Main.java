d2977b3439e7c5c2e4cc40158c7e0fb1a777afad
Douglas Raymond Siano <DouglasSiano@gmail.com>
2020-03-15 18:24:43 -0400

    fixed pom

--- src/test/java/swapirestvehicle/Main.java
+++ src/test/java/swapirestvehicle/Main.java
@@ -1,24 +1,14 @@
 package swapirestvehicle;
 
-import static io.restassured.RestAssured.*;
-//import static io.restassured.matcher.RestAssuredMatchers.*;
-//import static org.hamcrest.Matchers.*;
-//import static io.restassured.module.jsv.JsonSchemaValidator.*;
-
-// https://swapi.co/api/vehicles/
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class Main {
@@ -39,4 +29,16 @@ public class Main {
 				.assertThat().statusCode(200);
 	}
 
+	@Test
+	public void testKnownResponseContents() {
+		Response response = given().when().get("https://swapi.co/api/vehicles/4/");
+		String name = response.jsonPath().get("name");
+		assertEquals("Sand Crawler", name);
+	}
+
+	@Test
+	public void testPageNotFound() {
+		given().baseUri("https://swapi.co/api").contentType(ContentType.JSON).when().get("/vehicles/gsdfg/").then()
+				.assertThat().statusCode(404);
+	}
 }
