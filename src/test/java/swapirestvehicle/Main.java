724e2628543f5bbb55d98d86382e06e278d1229a
Douglas Raymond Siano <DouglasSiano@gmail.com>
2020-03-15 16:26:35 -0400

    JUnit 5 tests not found?

--- src/test/java/swapirestvehicle/Main.java
+++ src/test/java/swapirestvehicle/Main.java
@@ -9,6 +9,7 @@ import static io.restassured.module.jsv.JsonSchemaValidator.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
@@ -17,7 +18,25 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import io.restassured.http.ContentType;
+
 @Execution(ExecutionMode.CONCURRENT)
 public class Main {
+
+	/*
+	 * @BeforeAll public void commonSetUp() { // }
+	 */
+
+	@Test
+	public void firstTest() {
+		given().baseUri("https://swapi.co/api").contentType(ContentType.JSON).when().get("/vehicles/4/").then()
+				.assertThat().statusCode(200);
+	}
+
+	@Test
+	public void secondTest() {
+		given().baseUri("https://swapi.co/api").contentType(ContentType.JSON).when().get("/vehicles/8/").then()
+				.assertThat().statusCode(200);
+	}
 
 }
